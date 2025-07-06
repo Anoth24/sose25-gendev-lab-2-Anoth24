8316e9abd934d2463c19a9d5777acb016bfe5e75
Anoth24 <anoth.k.24@gmail.com>
2025-07-06 19:29:43 +0200

    Initial commit

--- src/main/java/gendev/lab2/SpecSimulatorCmd.java
+++ src/main/java/gendev/lab2/SpecSimulatorCmd.java
@@ -3,37 +3,58 @@ package gendev.lab2;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import tau.smlab.syntech.controller.executor.ControllerExecutor;
 import tau.smlab.syntech.games.controller.jits.BasicJitController;
 
-
 public class SpecSimulatorCmd {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> inputs = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
 
-		Map<String, String> inputs = new HashMap<>();
+        // Instantiate a new controller executor
+        ControllerExecutor executor = new ControllerExecutor(new BasicJitController(), "out/jit", "Spec");
 
-		// Instantiate a new controller executor
-		ControllerExecutor executor = new ControllerExecutor(new BasicJitController(), "out/jit", "Spec");
+        boolean iniState = true;
 
-		boolean iniState = true;
-		while (true) {
-			inputs.clear();
+        System.out.println("=*=*= Greenhouse Controller Simulation =*=*=");
+        System.out.println("Available environment inputs: tempHigh, soilDry, lightLow");
+        System.out.println("Enter true/false for each input (or just press Enter for false)");
 
-			// TODO read inputs here
+        while (true) {
+            inputs.clear();
 
-			// TODO send inputs to controller
+            //READ INPUTS
+            System.out.print("\nIs temperature high? (tempHigh) [true/false]: ");
+            String tempHigh = scanner.nextLine().trim();
+            if (tempHigh.equalsIgnoreCase("true")) inputs.put("tempHigh", "true");
 
-			// execute controller
-			if (iniState) {
-				executor.initState(inputs);
-				iniState = false;
-			} else {
-				executor.updateState(inputs);
-			}
+            System.out.print("Is soil dry? (soilDry) [true/false]: ");
+            String soilDry = scanner.nextLine().trim();
+            if (soilDry.equalsIgnoreCase("true")) inputs.put("soilDry", "true");
 
-			// TODO read outputs and decide how to proceed
-		}
-	}
+            System.out.print("Is light low? (lightLow) [true/false]: ");
+            String lightLow = scanner.nextLine().trim();
+            if (lightLow.equalsIgnoreCase("true")) inputs.put("lightLow", "true");
+
+            //SEND INPUTS TO CONTROLLER
+            if (iniState) {
+                executor.initState(inputs);
+                iniState = false;
+            } else {
+                executor.updateState(inputs);
+            }
+
+            //READ OUTPUTS
+            Map<String, String> outputs = executor.getCurrOutputs();
+
+            //PRINT SYSTEM OUTPUTS
+            System.out.println("\n--- System Actions ---");
+            System.out.println("Fan On:         " + outputs.getOrDefault("fanOn", "false"));
+            System.out.println("Water Pump On:  " + outputs.getOrDefault("waterPumpOn", "false"));
+            System.out.println("Grow Light On:  " + outputs.getOrDefault("growLightOn", "false"));
+        }
+    }
 }
